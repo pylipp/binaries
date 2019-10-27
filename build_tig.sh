9904540fd1179cc572ec392ff4a278869d5e8456
pylipp <philipp.metzner@tum.de>
2019-10-27 11:18:19 +0100

    Build tig from tarball
    Comes with man pages instead of adoc files.

--- build_tig.sh
+++ build_tig.sh
@@ -2,13 +2,13 @@
 
 set -ev
 
-git clone https://github.com/jonas/tig
-cd tig
-git checkout tig-2.4.1
+wget https://github.com/jonas/tig/releases/download/tig-2.4.1/tig-2.4.1.tar.gz
+tar xvf tig-2.4.1.tar.gz
+cd tig-2.4.1
 
 ./autogen.sh
 ./configure
 make prefix=~/.local
 
 # Bundle executable, completion scripts and man page
-tar -czf tig.tar.gz tig tig.1 tigmanual.7 tigrc.5 contrib/tig-completion.zsh contrib/tig-completion.bash
+tar -czf tig.tar.gz src/tig tig.1 tigmanual.7 tigrc.5 contrib/tig-completion.zsh contrib/tig-completion.bash
